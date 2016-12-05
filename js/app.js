f420cb5496927e7eb8a5476256e358dd041aad71
edprince <edward_prince@hotmail.com>
2016-12-05 11:50:49 +0000

    kumu map added

---
+++ js/app.js
@@ -0,0 +1,19 @@
+$('document').ready(() => {
+  $('#close_intro_modal').click(() => {
+    $('#intro_modal').hide();
+  })
+
+})
+$('#explore').click(() => {
+  insertData();
+})
+
+function insertData() {
+  //Remove Title
+  $('#title_div').hide();
+  $('#wrapper').hide();
+  //Insert kumu
+  $('#main_screen').show();
+  $('#intro_modal').show();
+  //Load controls
+}
